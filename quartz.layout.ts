455e7fa39f0bece39f8f85ea6029447382d00c74
Andreas Backström <andreas@simplymozzo.se>
2024-04-28 14:03:04 +0200

    Quartz sync: Apr 28, 2024, 2:03 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Website": "https://andreasbackstrom.se",
+      "Mastodon": "https://fosstodon.org/@mozzo",
     },
   }),
 }

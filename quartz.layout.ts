491e179508303114dd4f73574d54f83a38f8c12b
Andreas Backström <mozzo242@gmail.com>
2026-02-26 21:32:56 +0100

    Add links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,10 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Website": "https://andreasbackstrom.se/",
+      GitHub: "https://github.com/mozzo1000",
+      Mastodon: "https://fosstodon.org/@mozzo",
+
     },
   }),
 }
